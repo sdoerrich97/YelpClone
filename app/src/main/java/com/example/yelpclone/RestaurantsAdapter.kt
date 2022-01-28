cc094a4e8f41e6c0011ec2c30e43236b76fd173d
Schmieri <bob.schmierhose@gmail.com>
2022-01-28 16:53:06 -0500

    Rounding up the project

--- app/src/main/java/com/example/yelpclone/RestaurantsAdapter.kt
+++ app/src/main/java/com/example/yelpclone/RestaurantsAdapter.kt
@@ -2,15 +2,34 @@ package com.example.yelpclone
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaurant>) : RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRestaurant) {
             itemView.findViewById<TextView>(R.id.tvName).text = restaurant.name
+            itemView.findViewById<RatingBar>(R.id.ratingBar).rating = restaurant.rating.toFloat()
+            itemView.findViewById<TextView>(R.id.tvNumReviews).text = "${restaurant.numReviews} Reviews"
+            itemView.findViewById<TextView>(R.id.tvAddress).text = restaurant.location.address
+            itemView.findViewById<TextView>(R.id.tvCategory).text = restaurant.categories[0].title
+            itemView.findViewById<TextView>(R.id.tvDistance).text = restaurant.displayDistance()
+            itemView.findViewById<TextView>(R.id.tvPrice).text = restaurant.price
+
+            Glide.with(context).load(restaurant.imageUrl)
+                .apply(RequestOptions().transform(
+                    CenterCrop(), RoundedCorners(20)
+                ))
+                .into(itemView.findViewById(R.id.imageView))
         }
 
     }
